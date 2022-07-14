98dc8343815a225b2ae7a8b8805d94728bc0c3b4
Fabien <fabien.webdev@gmail.com>
2022-07-14 12:00:28 +0200

    add default structure

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}
---
+++ src/Datagrid/Datagrid.tsx
@@ -0,0 +1,7 @@
+import { DatagridProps } from './Datagrid.types'
+
+export default function Datagrid (props: DatagridProps) {
+  return (
+    <div>Datagrid</div>
+  )
+}
