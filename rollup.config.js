4ade798b1eb80384d2cf7070d445472a2493423c
Fabien <fabien.webdev@gmail.com>
2022-07-27 19:31:50 +0200

    clean code

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import dts from 'rollup-plugin-dts'
 import { terser } from 'rollup-plugin-terser'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
-const packageJson = require('./package.json')
+import packageJson from './package.json'
 
 export default [
   {
