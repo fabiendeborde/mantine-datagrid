f16f2ed3a9633ee19b9d984180f8362b5c14de5d
Fabien <fabien.webdev@gmail.com>
2022-07-29 18:29:05 +0200

    clean column filter

--- src/Datagrid/components/ColumnFilter.tsx
+++ src/Datagrid/components/ColumnFilter.tsx
@@ -3,68 +3,59 @@ import {
   Button,
   Group,
   Menu,
-  Select,
   Stack,
-  TextInput,
   useMantineTheme
 } from '@mantine/core'
 import { Column, ColumnFilter } from '@tanstack/react-table'
 import { Filter as FilterIcon } from 'tabler-icons-react'
-import { StringFilter } from './Filters/utils'
+
+import { FilterState } from '../Datagrid.types'
+import { isDataGridFilter } from '../filters/utils'
 
 type Props<T> = {
   column: Column<T, unknown>;
 }
 
-type FilterState = {
-  operator: StringFilter;
-  value: unknown;
-}
-
 export function ColumnFilter<T> ({ column }: Props<T>) {
   const theme = useMantineTheme()
   const [opened, setOpened] = useState(false)
-  const [filterState, setFilterState] = useState<FilterState>()
+  const [filterState, setFilterState] = useState<FilterState<unknown, unknown>>({ operator: undefined, value: undefined })
   const isFiltered = column.getIsFiltered()
 
   const filterFn = column.columnDef.filterFn
 
-  // if (!isDataGridFilter(filterFn)) return null;
-
-  // const { element: Element, init } = filterFn;
+  if (!isDataGridFilter(filterFn)) {
+    if (filterFn !== 'auto') {
+      console.warn(`Built-in filters are not supported by this library. \nYou must provide a filter function that implements 'DataGridFilterFn' type.\n (Received '${filterFn}' on column ${column.id})`)
+    }
+    return null
+  }
+  const { filterComponent: FilterComponent, initialFilter } = filterFn
 
   // console.log('column', column)
-  // console.log('filterFn', filterFn)
 
   const open = () => {
-    const defaultValue = column.getFilterValue() as FilterState || {
-      operator: StringFilter.Includes,
-      value: ''
-    }
+    const defaultValue = column.getFilterValue() as FilterState<unknown, unknown> || initialFilter()
 
     setFilterState(defaultValue)
   }
   const close = () => {
-    setFilterState(undefined)
+    setFilterState({ operator: undefined, value: undefined })
     setOpened(false)
   }
-  const valueChange = (value: unknown) => {
+  const onValueChange = (value: unknown) => {
     setFilterState(current => {
-      if (current) {
-        return {
-          ...current,
-          value
-        }
+      return {
+        ...current,
+        value
       }
     })
   }
-  const operatorChange = (operator: StringFilter) => {
+  const onOperatorChange = (operator: unknown) => {
     setFilterState(current => {
-      if (current) {
-        return {
-          ...current,
-          operator
-        }
+      return {
+        ...current,
+        operator
       }
     })
   }
@@ -96,23 +87,11 @@ export function ColumnFilter<T> ({ column }: Props<T>) {
       </Menu.Target>
       <Menu.Dropdown>
         <Stack py="sm" px="xs">
-          <Select
-            data={Object.entries(StringFilter).map(
-              ([label, value]) => ({
-                value,
-                label: label.replace(/([a-z]+)([A-Z]{1})/g, '$1 $2')
-              })
-            )}
-            value={filterState?.operator || StringFilter.Includes}
-            onChange={operatorChange}
+          <FilterComponent
+            filterState={filterState}
+            onValueChange={onValueChange}
+            onOperatorChange={onOperatorChange}
           />
-
-          <TextInput
-            value={filterState?.value as string || ''}
-            onChange={(e) => valueChange(e.target.value)}
-            placeholder="Filter value..."
-          />
-
           <Group position="apart">
             <Button size="xs" variant='subtle' color="gray" onClick={clear}>Clear</Button>
             <Button size="xs" onClick={save} leftIcon={<FilterIcon size={16} />}>Filter</Button>
