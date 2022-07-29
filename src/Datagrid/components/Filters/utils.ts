fd2295cc329dc6d32b91b46f6816174f021ff747
Fabien <fabien.webdev@gmail.com>
2022-07-29 10:37:33 +0200

    clean filter utilities

--- src/Datagrid/components/Filters/utils.ts
+++ src/Datagrid/components/Filters/utils.ts
@@ -1,37 +1,3 @@
-import { FilterFn } from '@tanstack/react-table'
-
 export const hasFilter = (filterFn: unknown) => {
   return filterFn === 'includesString'
 }
-
-export enum StringFilter {
-  Includes = 'in',
-  DoesNotInclude = 'nin',
-  Equals = 'eq',
-  DoesNotEqual = 'neq',
-  StartsWith = 'start',
-  EndsWith = 'end',
-}
-
-export const stringFilterFn: FilterFn<unknown> = (row, columnId, filter) => {
-  const rowValue = String(row.getValue(columnId)).toLowerCase()
-  const op = filter.op || StringFilter.Includes
-  const filterValue = String(filter.value).toLowerCase()
-  switch (op) {
-    case StringFilter.Includes:
-      return rowValue.includes(filterValue)
-    case StringFilter.DoesNotInclude:
-      return !rowValue.includes(filterValue)
-    case StringFilter.Equals:
-      return rowValue === filterValue
-    case StringFilter.DoesNotEqual:
-      return rowValue !== filterValue
-    case StringFilter.StartsWith:
-      return rowValue.startsWith(filterValue)
-    case StringFilter.EndsWith:
-      return rowValue.endsWith(filterValue)
-    default:
-      return true
-  }
-}
-stringFilterFn.autoRemove = (val) => !val
