9e054909ec83d5d61a6e1acc4f0d0fe519da7826
Fabien <fabien.webdev@gmail.com>
2022-07-29 18:49:23 +0200

    WIP: remove filter export (tmp fix)

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@
 export * from '@tanstack/react-table'
 export * from './Datagrid/Datagrid.types'
 export * from './Datagrid/Datagrid'
-export * from './Datagrid/filters'
 export * from './Checkbox/Checkbox'
